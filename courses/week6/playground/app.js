6f8c165985afa0bc721d2ff2c6e4f87c035a8c8f
Razvan <razvan.timis@aeratechnology.com>
2020-03-08 09:16:25 +0200

    Add playground

---
+++ courses/week6/playground/app.js
@@ -0,0 +1,13 @@
+console.log(document.querySelectorAll('*'))
+
+
+
+console.log(typeof 1)
+console.log(typeof [])
+console.log(typeof true)
+console.log(typeof { a: 1 })
+function returnTrue() {
+    return true;
+}
+
+console.log(typeof returnTrue())
