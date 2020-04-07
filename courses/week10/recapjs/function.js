8662ff30748c9f39f01bc38f3ab636b0fb5f9c2b
Razvan Timis <timisrazvanvasile@gmail.com>
2020-04-07 21:36:12 +0300

    Add more exemple

--- courses/week10/recapjs/function.js
+++ courses/week10/recapjs/function.js
@@ -1,4 +1,13 @@
-// scope
+// parameter
+
+function displayComments(comments, color) {
+
+}
+const comments = [];
+const color = "red";
+displayComments(comments, color);
+
+// scope - hosting
 var a = 10;
 function setNumber() {
     var a;
@@ -15,3 +24,5 @@ const makeAdderFn = function (x) {
         return x + y;
     }
 }
+const addFn = makeAdderFn(10);
+console(addFn(5))
