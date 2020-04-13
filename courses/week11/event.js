cd2c24a1667a26c5ec11b4eff2a220bed1d2934a
Razvan Timis <timisrazvanvasile@gmail.com>
2020-04-13 21:49:34 +0300

    Add comment

--- courses/week11/event.js
+++ courses/week11/event.js
@@ -1,3 +1,6 @@
+// callback function
 document.body.addEventListener('click', function () {
+  // this este contextul unde se excuta functie, 
+  // in cazul acesta va fi document.body deci this = document.body
   console.log(this);
 });
