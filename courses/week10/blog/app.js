b5caaa1dfb9651364f1466b8c1fcc1cc1a71ef34
Razvan Timis <timisrazvanvasile@gmail.com>
2020-04-13 18:52:29 +0300

    Add

--- courses/week10/blog/app.js
+++ courses/week10/blog/app.js
@@ -2,7 +2,6 @@
 // aici cream un obiect cu ajutorul clasei Post
 const post1 = new Post(1, "alex", "12/03/2020", "Titlu 1", "buna, primul post");
 const post2 = new Post(1, "razvan", "12/03/2020", "Titlu 2", " buna, a doi lea post");
-
 const com1 = new Comment(1, "razvan", "foarte tare")
 const com2 = new Comment(2, "alex", "foarte rau")
 
@@ -14,7 +13,7 @@ const listPost = [post1, post2]
 
 // afisam posturile in HTML
 for (let i = 0; i < listPost.length; i++) {
-    // obtinem reprezentarea postului din HTML
-    const postNode = listPost[i].display();
-    document.getElementById('listOfPost').appendChild(postNode);
+  // obtinem reprezentarea postului din HTML
+  const postNode = listPost[i].display();
+  document.getElementById('listOfPost').appendChild(postNode);
 }
