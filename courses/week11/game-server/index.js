89b182c900994cbd6006a9e428fa0d6011d9a92c
Razvan Timis <timisrazvanvasile@gmail.com>
2020-04-22 18:24:02 +0300

    Add game server

---
+++ courses/week11/game-server/index.js
@@ -0,0 +1,56 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+
+app.use(cors());
+app.use(bodyParser());
+
+router.get('/games', async ctx => {
+  ctx.body = db.get('games')
+});
+
+router.get('/games/:id', async ctx => {
+  ctx.body = db.get('games').find({ id: ctx.params.id });
+});
+
+
+router.post('/games', async ctx => {
+  const post = await db.get('games')
+    .push(ctx.request.body)
+    .last()
+    .assign({ id: Date.now().toString() })
+    .write()
+
+  ctx.body = post;
+});
+
+
+
+router.delete('/games/:id', async ctx => {
+  const posts = await db.get('games')
+    .remove({ id: ctx.params.id })
+    .write()
+  ctx.body = posts[0];
+})
+
+router.put('/games/:id', async ctx => {
+  const post = await db.get('games')
+    .find({ id: ctx.params.id })
+    .assign({ ...ctx.request.body })
+    .write()
+  ctx.body = post;
+})
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
