64fd304b1678b06410950344b4b30ceca22dcd0c
Razvan Timis <timisrazvanvasile@gmail.com>
2020-05-17 12:01:01 +0300

    Add p5

---
+++ courses/week16/final-exemple/my-react/p5/readme.md
@@ -0,0 +1,7 @@
+# Changelog
+
+- 01 Support `div` element
+- 02 Support other html elements
+- 03 Functional components
+- 04 Component composition
+- 05 Class components
---
+++ courses/week16/final-exemple/my-react/p5/src/react.js
@@ -0,0 +1,23 @@
+function create(type, children) {
+  if (typeof type === 'function') {
+    return type();
+  } else {
+    const element = document.createElement(type);
+    children.forEach(child => {
+      if (typeof (child) === 'object') {
+        element.appendChild(child);
+      } else {
+        element.innerHTML += child;
+      }
+    });
+    return element;
+  }
+}
+
+function createElement(type, props, ...children) {
+  return create(type, children);
+}
+
+window.React = {
+  createElement
+};
