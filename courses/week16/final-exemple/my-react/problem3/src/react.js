a60a72e9d1dbce4fd963956b383218d41e43fbd9
Razvan Timis <timisrazvanvasile@gmail.com>
2020-05-17 11:54:39 +0300

    Add pb3

---
+++ courses/week16/final-exemple/my-react/problem3/src/react.js
@@ -0,0 +1,13 @@
+function create(type, children) {
+  const element = document.createElement(type);
+  element.innerHTML = children.join(' ');
+  return element;
+}
+
+function createElement(type, props, ...children) {
+  return create(type, children);
+}
+
+window.React = {
+  createElement
+};
