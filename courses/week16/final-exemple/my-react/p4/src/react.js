5e56d941195b3a45fabb11b13eeceaffed056671
Razvan Timis <timisrazvanvasile@gmail.com>
2020-05-17 11:59:49 +0300

    Add p4

---
+++ courses/week16/final-exemple/my-react/p4/src/react.js
@@ -0,0 +1,16 @@
+function create(type, children) {
+  if (typeof type === 'function') {
+    return type();
+  } else {
+    const element = document.createElement(type);
+    element.innerHTML = children.join(' ');
+    return element;
+  }
+}
+
+function createElement(type, props, ...children) {
+  return create(type, children);
+}
+window.React = {
+  createElement
+};
