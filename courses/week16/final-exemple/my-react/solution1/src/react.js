7e0548f8fdcf69017a7d4c75258e2525973868ce
Razvan Timis <timisrazvanvasile@gmail.com>
2020-05-17 11:24:52 +0300

    Add sol1

---
+++ courses/week16/final-exemple/my-react/solution1/src/react.js
@@ -0,0 +1,13 @@
+function div(children) {
+  const aDiv = document.createElement('div');
+  aDiv.innerHTML = children.join(' ');
+  return aDiv;
+}
+
+function createElement(type, props, ...children) {
+  return div(children);
+}
+
+window.React = {
+  createElement
+};
