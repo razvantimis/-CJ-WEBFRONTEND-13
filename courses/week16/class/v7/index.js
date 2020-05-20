98b1d0cc857e9d0f83976c2a01585ecaadd755ec
Razvan Timis <timisrazvanvasile@gmail.com>
2020-05-20 20:17:36 +0300

    Add v7

---
+++ courses/week16/class/v7/index.js
@@ -0,0 +1,34 @@
+
+function Post(props) {
+  const myStyle = {
+    marginTop: "10px"
+  }
+  console.log(props)
+
+  return (
+    <div className="post" style={myStyle}>
+      <h1>{props.title}</h1>
+      <p>{props.content.join(' ')}</p>
+    </div>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    const content = ['text1', 'text2']
+    return (
+      <div>
+        <Post title="title1" content={content} />
+        <Post title="title2" content={content} />
+      </div>
+    )
+  }
+}
+
+const appDOM = document.getElementById('app');
+ReactDOM.render(<App />, appDOM)
+
+
+
+
+
